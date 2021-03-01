d1410a7c0b9c79fb5a17f8603b2e956d480ec45a
kos <liskosnik@yandex.ru>
2021-03-01 17:33:07 +0300

    на уроке вариант 12

--- variant_12/6.py
+++ variant_12/6.py
@@ -7,8 +7,6 @@ def f(d):
     return n
 
 
-k = 0
 for i in range(1, 100000):
     if f(i)==89:
-        k += 1
-print(k)
+        print(i)
--- variant_12/8.py
+++ variant_12/8.py
@@ -2,8 +2,6 @@ k = 0
 for s1 in "АБВГ":
     for s2 in "АБВГ":
         for s3 in "АБВГ":
-            for s4 in "АБВГ":
-                s = s1 + s2 + s3 + s4
-                if s1 != 'Г':
-                    k+=1
-print(k)
+            s = s1 + s2 + s3
+            k += 1
+            print(k, s)
